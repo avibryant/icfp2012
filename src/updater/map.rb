c60d07a41caaf8b301e6e9306af5773740947106
Steven Noble <steven.noble@gmail.com>
2012-07-13 11:02:07 -0400

    updates the rocks one round. current map is in the source file and needs to be moved to stdin. updateRobot and updateLambdaLift need to be implemented

--- src/updater/map.rb
+++ src/updater/map.rb
@@ -3,25 +3,68 @@ class LambdaMap
     <<-STR
 ######
 #. *R#
-#  \\.#
+#  *.#
 #\\ * #
 L  .\\#
 ######
 STR
   end
 
-  def printMap
-    puts @map.reverse
+  def initialize
+    populateMap
+    @writeMap = @clone
+  end
+
+  def clone
+    ClonedLambdaMap.new(@map)
+  end
+
+  def width
+    @widthCache ||= @map.map {|row| row.length}.max
+  end
+
+  def to_s
+    @map.reverse.inject(''){|str, row| str + "\n" + row}
+  end
+
+  def set(x,y,char)
+    @map[y][x] = char
   end
 
   def populateMap
     @map = mapString.split("\n").reverse
   end
 
-  def get(x,y)
-    @map[y][x] || ' '
+  def rocks
+    ret = []
+    @map.each_with_index do |row, y|
+      row.chars.with_index(0).each do |c, x| 
+        ret.push Rock.new(self,x,y) if c == '*'
+      end
+    end
+    ret
   end
 
+  def [](x,y)
+    return '#' if @map[y] == nil
+    @map[y][x] || ((x < 0 or x >= @width) ? '#' : ' ')
+  end
+
+  def updateRobot(move)
+  end
+
+  def updateRocks
+    writeMap = clone
+    rocks.each {|rock| rock.update(writeMap)}
+    writeMap
+  end
+
+end
+
+class ClonedLambdaMap < LambdaMap
+  def initialize(map)
+    @map = map.map {|row| row.clone}
+  end
 end
 
 class Robot
@@ -29,9 +72,64 @@ class Robot
 end
 
 class Rock
+  def initialize (readMap, x, y)
+    @readMap = readMap
+    @x = x
+    @y = y
+  end
+  
+  def to_s
+    "#{@x},#{@y}"
+  end
+
+  def s
+    @readMap[@x, @y - 1]
+  end
+
+  def e
+    @readMap[@x + 1, @y]
+  end
+
+  def w
+    @readMap[@x - 1, @y]
+  end
+
+  def se
+    @readMap[@x + 1, @y - 1]
+  end
+
+  def sw
+    @readMap[@x - 1, @y - 1]
+  end
+
+  def update(writeMap)
+    if s == ' '
+      moveS(writeMap)
+    elsif s == '*'
+      if e == ' ' and se == ' '
+        moveSE(writeMap)
+      elsif w == ' ' and sw == ' '
+        moveSW(writeMap)
+      end
+    end
+  end
+
+  def moveS(writeMap)
+    writeMap.set(@x, @y, ' ')
+    writeMap.set(@x, @y - 1, '*')
+  end
+
+  def moveSE(writeMap)
+    writeMap.set(@x, @y, ' ')
+    writeMap.set(@x + 1, @y - 1, '*')
+  end
+
+  def moveSW(writeMap)
+    writeMap.set(@x, @y, ' ')
+    writeMap.set(@x - 1, @y - 1, '*')
+  end
 
 end
 
-foo = LambdaMap.new()
-foo.populateMap
-foo.printMap
+map = LambdaMap.new()
+puts map.updateRocks
