1040b1d74a8d65648f4ca821a5d2c448680b5e8e
Avi Bryant <avi@avibryant.com>
2012-07-13 21:10:16 -0700

    instrument the tree walker

--- src/updater/tree.rb
+++ src/updater/tree.rb
@@ -3,10 +3,15 @@ require 'map'
 class Tree
   DIRECTIONS = ["U", "D", "L", "R", "W", "A"]
 
-  attr_reader :leaves
+  attr_reader :leaves, :move_robot_time, :move_rocks_time, :score_time, :sort_time, :leaves_considered
 
   def initialize(map)
     @leaves = {"" => map}
+    @move_robot_time = 0
+    @move_rocks_time = 0
+    @score_time = 0
+    @sort_time = 0
+    @leaves_considered = 0
   end
 
   def iterate
@@ -16,7 +21,15 @@ class Tree
         new_leaves[moves] = leaf
       else
         DIRECTIONS.each do |dir|
-          new_leaves[moves + dir] = leaf.command_robot(dir).move_rocks
+          t1 = Time.new.to_f
+          mx = leaf.command_robot(dir)
+          t2 = Time.new.to_f
+          mr = mx.move_rocks
+          t3 = Time.new.to_f
+          new_leaves[moves + dir] = mx
+          @leaves_considered += 1
+          @move_robot_time += (t2 - t1)
+          @move_rocks_time += (t3 - t2)
        end
       end
     end
@@ -25,9 +38,11 @@ class Tree
 
   def top(n)
     scores = {}
+    t1 = Time.new.to_f
     @leaves.each do |moves, leaf|
       scores[moves] = leaf.score
     end
+    t2 = Time.new.to_f
 
     sorted_moves = scores.keys.shuffle.sort{|a,b| scores[a] <=> scores[b]}
     best_done = sorted_moves.reverse.find{|m| @leaves[m].is_done?}
@@ -35,6 +50,10 @@ class Tree
     if(sorted_moves.size> n)
       sorted_moves = sorted_moves[-1 * n .. -1]
     end
+    t3 = Time.new.to_f
+
+    @score_time += (t2 - t1)
+    @sort_time += (t3 - t2)
     sorted_moves.map{|m| [m, scores[m]]}
   end
 
@@ -68,7 +87,10 @@ iterations.times do |i|
   tree.prune(prune)
   puts "Iteration #{i+1}"
   puts "Leaves: #{tree.leaves.size}"
+  puts "Leaves considered: #{tree.leaves_considered}"
   moves, score = tree.top(1)[0]
   puts "Best score: #{score}"
   puts "Best moves: #{moves}"
+  puts "Move robot time: #{tree.move_robot_time}"
+  puts "Move rocks time: #{tree.move_rocks_time}"
 end
