fe47cfd2b1ebcdf20cc42b1eaa4dd09843ed5aa7
Steven Noble <steven.noble@gmail.com>
2012-07-15 00:08:48 -0400

    helps visualizing the heatmap in fastmap

---
+++ src/fast/foo.rb
@@ -0,0 +1,16 @@
+require '../fast/map'
+
+map = FastMap.new(STDIN.read.split("\n"))
+map.create_heatmap!
+map2 = map
+heatmap = []
+map2.heatmap.each do |k, v|
+  heatmap[k[1]] = [] if heatmap[k[1]] == nil
+  heatmap[k[1]][k[0]] = v
+end
+puts heatmap.map {|r| r.join(",")}.join("\n")
+puts map2.value
+puts map2.fixed_find('R').join(",")
+
+puts map2.best_moves
+puts map2.to_s
