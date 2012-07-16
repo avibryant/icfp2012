fb6ee1945fc5a183dba82e965f2cf8b22fa34147
Avi Bryant <avi@avibryant.com>
2012-07-16 01:12:53 -0700

    Fix to pkg script

--- pkg.sh
+++ pkg.sh
@@ -7,7 +7,8 @@ cp lifter pkg/
 cp install pkg/
 cp PACKAGES pkg/
 cp README pkg/
+cp -r src/main/scala pkg/src
 cd pkg
-tar -cvzf barbers.tgz *
+tar -cvzf icfp-96223304.tgz *
 cd ..
 mv pkg/icfp-96223304.tgz ./
